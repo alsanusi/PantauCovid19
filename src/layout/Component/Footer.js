9c91cfe345669456da7e17270e6c654177c4ae82
Als <malkautsars@gmail.com>
2020-03-28 17:05:17 +0800

    fix: seperate Footer to component

---
+++ src/layout/Component/Footer.js
@@ -0,0 +1,37 @@
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core';
+import { BottomNavigation, BottomNavigationAction } from '@material-ui/core/';
+
+const footerStyles = makeStyles( theme => ({
+    root: {
+      width: '100%',
+      position: 'fixed',
+      bottom: 0
+    },
+    label: {
+      fontWeight: 'bold',
+    },
+    selected: {
+      width: "max-content",
+    }
+  }));
+  
+export default function Footer() {
+    const classes = footerStyles();
+    const [value, setValue] = React.useState(0);
+  
+    return (
+      <BottomNavigation
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+          window.open("https://muhalkautsarsanusi.com/", "Author")
+        }}
+        showLabels
+        className={classes.root}
+      >
+        <BottomNavigationAction label="© Muhammad Alkautsar Sanusi." classes={{ root: classes.label, selected: classes.selected}}/>
+      </BottomNavigation>
+    );
+}
--- src/layout/IndonesiaDashboard.js
+++ src/layout/IndonesiaDashboard.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core';
-import { Grid, BottomNavigation, BottomNavigationAction } from '@material-ui/core/';
+import { Grid } from '@material-ui/core/';
 import Menu from './Component/Menu';
 import IndonesiaTable from './IndonesiaComponent/IndonesiaTable';
 import IndonesiaSummaryData from './IndonesiaComponent/IndonesiaCard';
 import Header from './Component/Header';
+import Footer from './Component/Footer';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -19,39 +20,6 @@ const useStyles = makeStyles(theme => ({
     color: theme.palette.text.secondary,
   },
 }));
-
-const footerStyles = makeStyles( theme => ({
-  root: {
-    width: '100%',
-    position: 'fixed',
-    bottom: 0
-  },
-  label: {
-    fontWeight: 'bold',
-  },
-  selected: {
-    width: "max-content",
-  }
-}));
-
-function Footer() {
-  const classes = footerStyles();
-  const [value, setValue] = React.useState(0);
-
-  return (
-    <BottomNavigation
-      value={value}
-      onChange={(event, newValue) => {
-        setValue(newValue);
-        window.open("https://muhalkautsarsanusi.com/", "Author")
-      }}
-      showLabels
-      className={classes.root}
-    >
-      <BottomNavigationAction label="© Muhammad Alkautsar Sanusi." classes={{ root: classes.label, selected: classes.selected}}/>
-    </BottomNavigation>
-  );
-}
 
 export default function CenteredGrid() {
   const classes = useStyles();

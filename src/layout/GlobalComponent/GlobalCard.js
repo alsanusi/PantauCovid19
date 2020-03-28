44da57803cfbe710717f809208f90501c8dcfaca
Als <malkautsars@gmail.com>
2020-03-28 16:39:36 +0800

    fix: Global Card Color

--- src/layout/GlobalComponent/GlobalCard.js
+++ src/layout/GlobalComponent/GlobalCard.js
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
+import { makeStyles, useTheme } from '@material-ui/styles';
 import Grid from '@material-ui/core/Grid';
 import { Card, CardContent, Typography } from '@material-ui/core';
 import CoronaApi from '../../api/CoronaApi';
@@ -8,44 +7,43 @@ import NotInterestedIcon from '@material-ui/icons/NotInterested';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
 
-const basicStyles = makeStyles(theme => ({
+const basicStyles = theme => ({
   root: {
     flexGrow: 1,
+    paddingLeft: '50%'
   },
   paperCard: {
-    paddingTop: theme.spacing(2),
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(2),
     borderRadius: 0, 
-    boxShadow: "none",
-    backgroundColor: '#F5F6FA'
+    maxWidth: 400,
+    borderTop: `4px solid`,
   },
   paperCardHeader: {
     borderRadius: 0, 
     boxShadow: "none",
   },
-  typography: {
+  header: {
     fontWeight: "bold", 
     textAlign: "left"
-  }
-}));
-
-const cardStyles = makeStyles({
-  root: {
-    maxWidth: 400,
-    borderTop: `4px solid`
   },
   title: {
     textAlign: "left",
   },
+  basicPadding: {
+    paddingTop: '16px',
+    paddingLeft: '16px',
+    paddingRight: '16px'
+  }
 });
 
-function CardDetail({ ...props }) {
-  const classes = cardStyles();
+function CardDetail({ color, ...props }) {
+  const theme = useTheme();
+  const styles = basicStyles(theme);
+  styles.paperCard.borderTopColor = color;
+  const classes = makeStyles(styles)();
 
   return (
     <div>
-    <Card className={classes.root}>
+    <Card className={classes.paperCard}>
       <CardContent>
         <div style={{display: "flex"}}>
           <div style={{width: "30%", alignSelf: "center"}}>
@@ -95,25 +93,19 @@ export default function Dashboard() {
 
   return (
     <div className={classes.root}>
-     <Grid container direction="column" justify="space-between">
-        <Grid item md xs={12}>
-          <Paper className={classes.paperCard} style={{marginBottom: '20px'}}>
-           <Typography variant="subtitle1" className={classes.typography}>
+     <Grid container direction="column" justify="space-between" style={{paddingTop: '16px', paddingLeft: '16px', paddingRight: '16px'}}>
+        <Grid item md xs={12} style={{marginBottom: '20px'}}>
+           <Typography variant="subtitle1" style={{fontWeight: 'bold'}}>
               {"Current Global Status."}
             </Typography>
             <br/>
-            <CardDetail img={<PeopleAltIcon style={{fontSize: 45}}/>} value={globalData.confirmed ? globalData.confirmed.toLocaleString() : 0} description={"Total Confirmed Cases."} />
-          </Paper>
+            <CardDetail img={<PeopleAltIcon style={{fontSize: 45}}/>} value={globalData.confirmed ? globalData.confirmed.toLocaleString() : 0} description={"Total Confirmed Cases."} color={"#0F4DB2"} />
         </Grid>
         <Grid item md xs={12}>
-          <Paper className={classes.paperCard}>
-            <CardDetail img={<FavoriteIcon style={{fontSize: 45}}/>} value={globalData.recovered ? globalData.recovered.toLocaleString() : 0} description={"Total People Recovered."} />
-          </Paper>
+            <CardDetail img={<FavoriteIcon style={{fontSize: 45}}/>} value={globalData.recovered ? globalData.recovered.toLocaleString() : 0} description={"Total People Recovered."} color={"#4A90E2"} />
         </Grid>
         <Grid item md xs={12}>
-          <Paper className={classes.paperCard}>
-            <CardDetail img={<NotInterestedIcon style={{fontSize: 45}}/>} value={globalData.deaths ? globalData.deaths.toLocaleString() : 0} description={"Total People Death."} />
-          </Paper>
+            <CardDetail img={<NotInterestedIcon style={{fontSize: 45}}/>} value={globalData.deaths ? globalData.deaths.toLocaleString() : 0} description={"Total People Death."} color={"#0F4DB2"} />
         </Grid>
       </Grid>
     </div>
